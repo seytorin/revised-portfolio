8bef911c88987c083dd12f1e7363270d61de3055
Your Name <seytorin@yahoo.com>
2018-03-16 23:37:08 -0500

    init files

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -15,13 +15,11 @@ module.exports = function(app) {
   // In each of the below cases the user is shown an HTML page of content
   // ---------------------------------------------------------------------------
 
-  app.get("/home", function (req, res) {
+  app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "../index.html"));
   });
 
   // If no matching route is found default to home
-  app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-  });
+ 
 };
   
